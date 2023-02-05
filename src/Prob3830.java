71add25edcbe700c9370f0e61960158db2568a47
KGW2027 <retryer_@naver.com>
2023-02-05 16:56:35 +0900

    최적화 3830

--- src/Prob3830.java
+++ src/Prob3830.java
@@ -18,8 +18,13 @@ public class Prob3830 {
 
                     if(ap == bp) continue; // 무게간 상대성을 깨트리는 링크는 존재하지 않는다. ( 또는 사이클을 이루는 경우 변화 없음 )
 
-                    parent_cost[bp][0] = a;
-                    parent_cost[bp][1] = w - parent_cost[b][1];
+                    // bp는 B보다 parent_cost[b][1]만큼 무겁다.
+                    // ap는 A보다 parent_cost[a][1]만큼 무겁다.
+                    // B는 A보다 W만큼 무겁다.
+                    // 즉, bp는 (B-parent_cost[b][1]) 이고, B는 parent_cost[a][1]+W이므로,
+                    // bp = (parent_cost[a][1] + W - parent_cost[b][1]
+                    parent_cost[bp][0] = ap;
+                    parent_cost[bp][1] = parent_cost[a][1] + w - parent_cost[b][1];
 
                 } else {
                     out.append( (ap!=bp) ? "UNKNOWN" : (parent_cost[b][1] - parent_cost[a][1])).append('\n');
